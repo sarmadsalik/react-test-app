7334b576a393a6d016c063b159252577e5474715
Sarmad Salik <sarmadsalikshafi@gmail.com>
2022-03-14 18:22:21 +0500

    react tutorial

---
+++ 01-tutorial/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       <div>Testing 01 tutorial</div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
